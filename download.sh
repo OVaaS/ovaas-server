9f4a01f2c97ab3e0477ed3217cbf0faf76513d9c
hanlee <me@hanlee.co>
2021-01-22 16:55:50 +0900

    fix: file path

--- download.sh
+++ download.sh
@@ -9,8 +9,8 @@ HUMAN_POSE=human-pose-estimation-0001
 curl --create-dirs \
 $URL/$HUMAN_POSE/FP32/$HUMAN_POSE.xml \
 $URL/$HUMAN_POSE/FP32/$HUMAN_POSE.bin \
--o models/human-pose-estimation/1/$HUMAN_POSE.xml \
--o models/human-pose-estimation/1/$HUMAN_POSE.bin
+-o ./models/human-pose-estimation/1/$HUMAN_POSE.xml \
+-o ./models/human-pose-estimation/1/$HUMAN_POSE.bin
 
 # Download handwritten-japanese-recognition model
 HANDWRITTEN=handwritten-japanese-recognition-0001
@@ -18,5 +18,5 @@ HANDWRITTEN=handwritten-japanese-recognition-0001
 curl --create-dirs \
 $URL/$HANDWRITTEN/FP32/$HANDWRITTEN.xml \
 $URL/$HANDWRITTEN/FP32/$HANDWRITTEN.bin \
--o models/handwritten-japanese-recognition/1/$HANDWRITTEN.xml \
--o models/handwritten-japanese-recognition/1/$HANDWRITTEN.bin
+-o ./models/handwritten-japanese-recognition/1/$HANDWRITTEN.xml \
+-o ./models/handwritten-japanese-recognition/1/$HANDWRITTEN.bin
