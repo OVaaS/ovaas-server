709d5f17ac3939231c98cd5e6f81f2172cf4a899
hanlee <me@hanlee.co>
2021-01-22 17:11:51 +0900

    fix: apply permission

--- install_debian.sh
+++ install_debian.sh
@@ -29,3 +29,5 @@ sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo su $USER
