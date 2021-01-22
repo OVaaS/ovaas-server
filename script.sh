39bac3fb48127f7dedc16ef4611002d842f0bed7
hanlee <me@hanlee.co>
2021-01-22 15:51:58 +0900

    fix: change docker compose to container version

--- script.sh
+++ script.sh
@@ -21,6 +21,6 @@ sudo apt-get update  -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L --fail https://github.com/docker/compose/releases/download/1.28.0/run.sh -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
