6f16219fb69fb737ebfa6906c97859bc68c5c3f2
hanlee <me@hanlee.co>
2021-01-22 13:20:23 +0900

    feat: docker compose for model server

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Install for using apt-get over https
 sudo apt-get update -y
 sudo apt-get install -y \
     apt-transport-https \
@@ -5,11 +8,26 @@ sudo apt-get install -y \
     curl \
     gnupg-agent \
     software-properties-common
+# Add docker GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
+# Add Docker repo
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+# Install docker
 sudo apt-get update  -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Download models
+sh ./download.sh
+
+# Run model server
+docker-compose up -d
+# See server logs: docker-compose logs
