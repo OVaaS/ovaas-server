28857855f066346083fa6fa3154c3eb6f56ea6b8
hanlee <me@hanlee.co>
2021-01-22 23:06:38 +0900

    feat: colorization

--- build_colorizetion_model.sh
+++ build_colorizetion_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -it -u 0 --rm -v $(pwd)/temp:/opt/intel/openvino_2021.2.185/temp  openvino/ubuntu18_dev:latest \
-python3 $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/downloader.py --list $INTEL_OPENVINO_DIR/inference_engine/demos/python_demos/colorization_demo/models.lst &&\
+docker run -it -u 0 --rm -v $(pwd)/temp:/opt/intel/openvino_2021.2.185/temp  openvino/ubuntu18_dev:latest  /bin/bash -c \
+"python3 $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/downloader.py --list $INTEL_OPENVINO_DIR/inference_engine/demos/python_demos/colorization_demo/models.lst &&\
 python3 $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/converter.py --name colorization-v2 &&\
-cp $INTEL_OPENVINO_DIR/public/colorization-v2/FP32/* $INTEL_OPENVINO_DIR/temp
+cp $INTEL_OPENVINO_DIR/public/colorization-v2/FP32/* $INTEL_OPENVINO_DIR/temp"
--- download.sh
+++ download.sh
@@ -20,3 +20,9 @@ $URL/$HANDWRITTEN/FP32/$HANDWRITTEN.xml \
 $URL/$HANDWRITTEN/FP32/$HANDWRITTEN.bin \
 -o ./models/handwritten-japanese-recognition/1/$HANDWRITTEN.xml \
 -o ./models/handwritten-japanese-recognition/1/$HANDWRITTEN.bin
+
+# Download and convert colorization model
+bash ./build_colorizetion_model.sh
+mkdir -p ./models/colorization-v2/1
+mv ./temp/* ./models/colorization-v2/1
+rm -r ./temp
