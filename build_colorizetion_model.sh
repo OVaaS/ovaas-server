a1403119432ef1edf09c7db4ad03358326a077f1
hanlee <me@hanlee.co>
2021-01-24 17:30:33 +0900

    fix: version

--- build_colorizetion_model.sh
+++ build_colorizetion_model.sh
@@ -2,7 +2,7 @@
 
 docker run -it -u 0 --rm \
 -v $(pwd)/temp:/opt/intel/openvino_2021.2.185/temp \
-openvino/ubuntu18_dev:latest bash -c \
+openvino/ubuntu18_dev:2021.1 bash -c \
 "python3 ./deployment_tools/tools/model_downloader/downloader.py --list ./inference_engine/demos/python_demos/colorization_demo/models.lst && \
 python3 ./deployment_tools/tools/model_downloader/converter.py --name colorization-v2 && \
 cp ./public/colorization-v2/FP32/* ./temp"
