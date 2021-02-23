33005c1ca07709a24c714aff28bac360a199963d
sadafa0077 <57556040+sadafa0077@users.noreply.github.com>
2021-02-23 23:39:34 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,3 +29,27 @@ sudo usermod -aG docker $USER
 sudo curl -L --fail https://github.com/docker/compose/releases/download/1.28.0/run.sh -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Download url
+URL=https://download.01.org/opencv/2020/openvinotoolkit/2020.4/open_model_zoo/models_bin/1
+
+# Download human-pose-estimation model
+HUMAN_POSE=human-pose-estimation-0001
+
+curl --create-dirs \
+$URL/$HUMAN_POSE/FP32/$HUMAN_POSE.xml \
+$URL/$HUMAN_POSE/FP32/$HUMAN_POSE.bin \
+-o ./models/human-pose-estimation/1/$HUMAN_POSE.xml \
+-o ./models/human-pose-estimation/1/$HUMAN_POSE.bin
+
+# Download handwritten-japanese-recognition model
+HANDWRITTEN=handwritten-japanese-recognition-0001
+
+curl --create-dirs \
+$URL/$HANDWRITTEN/FP32/$HANDWRITTEN.xml \
+$URL/$HANDWRITTEN/FP32/$HANDWRITTEN.bin \
+-o ./models/handwritten-japanese-recognition/1/$HANDWRITTEN.xml \
+-o ./models/handwritten-japanese-recognition/1/$HANDWRITTEN.bin
+
+# Download and convert colorization model
+bash ./build_colorizetion_model.sh
