1ca0b7d148d8cd69558a9c6828f454f944a11f5e
sadafa0077 <57556040+sadafa0077@users.noreply.github.com>
2021-02-23 23:40:25 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -52,4 +52,14 @@ $URL/$HANDWRITTEN/FP32/$HANDWRITTEN.bin \
 -o ./models/handwritten-japanese-recognition/1/$HANDWRITTEN.bin
 
 # Download and convert colorization model
-bash ./build_colorizetion_model.sh
+
+docker run -it -u 0 --rm \
+-v $(pwd)/temp:/opt/intel/openvino_2021.1.110/temp \
+openvino/ubuntu18_dev:2021.1 bash -c \
+"python3 ./deployment_tools/tools/model_downloader/downloader.py --list ./inference_engine/demos/python_demos/colorization_demo/models.lst && \
+python3 ./deployment_tools/tools/model_downloader/converter.py --name colorization-v2 && \
+cp ./public/colorization-v2/FP32/* ./temp"
+
+mkdir -p ./models/colorization-v2/1
+mv ./temp/* ./models/colorization-v2/1
+rm -r ./temp
